2315958a0044c336822d840300c2c0beaa33d842
Ravishanker V.G <no.1opensourcelover@protonmail.com>
2022-12-03 22:40:36 +0530

    JsSnakeGame: Initial Commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const items = document.getElementsByClassName("shape");
+
+const move = (item, x, y) => {
+    item.style.left = `${x}px`;
+    item.style.top = `${y}px`;
+}
+
+// Logic to check if the shape catches food
+const catchFood = (l1, l2, t1, t2) => {
+    return  l2 >= l1-50 && l2 <= l1+50 && t2 >= t1-25 && t2 <= t1+25;
+}
+
+let points = 0;
+
+const screenWidth=window.innerWidth;
+const screenHeight=window.innerHeight;
+
+const food = document.getElementsByClassName("food")[0];
+
+let leftPx = 80;
+let topPx = 50;
+
+food.style.left = `${leftPx}px`;
+food.style.top = `${topPx}px`;
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+window.onmousemove = e => {
+    for(i=0; i<items.length; i++){
+        move(items[i], e.clientX, e.clientY);
+    }
+
+    let collide = catchFood(e.clientX, leftPx, e.clientY, topPx);
+
+    if(collide){
+        leftPx = randomNumber(20,screenWidth-20);
+        topPx = randomNumber(20,screenHeight-20);
+        food.style.left = `${leftPx}px`;
+        food.style.top = `${topPx}px`;
+        points+=1;
+        document.getElementById("points").innerHTML=points;
+    }
+}
